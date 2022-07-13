0294835035da1fede5da52da86c4a8d47e8388c3
Lime <98422417+LimeAndPyro@users.noreply.github.com>
2022-07-13 05:26:31 -0400

    Add files via upload

---
+++ LoadSprite.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+using UnityEngine;
+
+namespace LoadSprite
+{
+    internal static class LoadSprite
+    {
+        internal static UnityEngine.Sprite LoadSpriteFromDisk(this string path)
+        {
+            byte[] array = File.ReadAllBytes(path);
+            Texture2D texture2D = new Texture2D(256, 256);
+            ImageConversion.LoadImage(texture2D, array);
+            Rect rect = new Rect(0.0f, 0.0f, texture2D.width, texture2D.height);
+            Vector2 vector = new Vector2(0.5f, 0.5f);
+            Vector4 zero = Vector4.zero;
+            return Sprite.CreateSprite_Injected(texture2D, ref rect, ref vector, 100f, 0, SpriteMeshType.Tight, ref zero, false);
+        }
+    } 
+}
---
+++ Main.cs
@@ -0,0 +1,44 @@
+﻿using LoadSprite;
+using MelonLoader;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Mic_Sprite_Changer
+{
+    public class MicChanger : MelonMod
+    {
+
+        public override void OnApplicationStart()
+        {
+
+            
+
+
+
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("..............................Are You Lost?......");
+            Console.WriteLine("I Updated it lol");
+            Console.WriteLine("lol pyro/lime/creed");
+
+        }
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            var MicIconMute = (Environment.CurrentDirectory + "\\MicIcons\\Mute.png").LoadSpriteFromDisk();
+            var MicIcon = (Environment.CurrentDirectory + "\\MicIcons\\Unmute.png").LoadSpriteFromDisk();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Loading Mic");
+            GameObject.Find("UserInterface/UnscaledUI/HudContent_Old/Hud/VoiceDotParent/VoiceDotDisabled").GetComponent<UnityEngine.UI.Image>().overrideSprite = MicIconMute;
+            GameObject.Find("UserInterface/UnscaledUI/HudContent_Old/Hud/VoiceDotParent/VoiceDot").GetComponent<UnityEngine.UI.Image>().overrideSprite = MicIcon;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Mic Loaded");
+        }
+
+    }
+}
+   
